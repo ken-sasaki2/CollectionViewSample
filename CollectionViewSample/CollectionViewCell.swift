159aeb873f04f7f8a74744c08b1e37c6d7e9d65c
佐々木　謙 <nkekisasa222@gmail.com>
2023-08-29 20:51:57 +0900

    add: cell追加

---
+++ CollectionViewSample/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewSample
+//
+//  Created by sasaki.ken on 2023/08/29.
+//
+
+import UIKit
+
+final class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
